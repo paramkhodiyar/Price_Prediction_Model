688fa0b6725a87496ae8f22aa956bb725e3f17e8
Param Khodiyar <paramkhodiyar1008@gmail.com>
2026-04-16 11:09:15 +0530

    Cleaning

--- agent/graph.py
+++ agent/graph.py
@@ -20,8 +20,6 @@ def fetch_prediction(state: AgentState) -> AgentState:
         model_path = os.path.join(os.path.dirname(__file__), "..", "models", "mayaai_sale_rf_model.pkl")
         with open(model_path, "rb") as f:
             model = pickle.load(f)
-        
-        # Try standard array or dict passing
         try:
             pred = model.predict([list(details.values())])
         except:
